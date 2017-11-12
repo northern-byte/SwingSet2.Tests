82da1b26245031e3a7dbaa694aa30491c588c7ac
Timur Barblishvili <unknitsplash@hotmail.com>
2017-11-12 16:39:23 +0300

    Init applet in EDT and clean up
    Init applet in EDT and then unload it and clean up the frame
    Wait for an applet to close. Wait has a timeout, specified by ‘disposeTimeout’ property in config file

--- src/main/java/implementations/fixtures/AppletSetupFixture.java
+++ src/main/java/implementations/fixtures/AppletSetupFixture.java
@@ -7,18 +7,30 @@ import interfaces.factories.ViewFactory;
 import interfaces.fixtures.SetupFixture;
 import interfaces.pageObjects.View;
 import org.fest.swing.applet.AppletViewer;
+import org.fest.swing.edt.GuiActionRunner;
+import org.fest.swing.edt.GuiQuery;
 import org.fest.swing.fixture.FrameFixture;
 import org.fest.swing.launcher.AppletLauncher;
+import org.fest.swing.timing.Condition;
+import org.fest.swing.timing.Pause;
+import org.fest.swing.timing.Timeout;
+import utils.ResourceManager;
 
 public class AppletSetupFixture implements SetupFixture {
 
+    private AppletViewer applet;
     private FrameFixture frame;
     private ViewFactory factory;
 
     @Override
     public View init() {
-        AppletViewer viewer = AppletLauncher.applet("SwingSet2Applet").start();
-        frame = new FrameFixture(viewer);
+        applet = GuiActionRunner.execute(new GuiQuery<AppletViewer>() {
+            @Override
+            protected AppletViewer executeInEDT() throws Throwable {
+                return AppletLauncher.applet("SwingSet2Applet").start();
+            }
+        });
+        frame = new FrameFixture(applet);
         factory = new BasicViewFactory(frame);
         return new DefaultView(factory);
     }
@@ -26,7 +38,15 @@ public class AppletSetupFixture implements SetupFixture {
     @Override
     public void dispose(){
         try {
-            frame.close();
+            applet.unloadApplet();
+            frame.cleanUp();
+
+            Pause.pause(new Condition("Check if applet is active") {
+                @Override
+                public boolean test() {
+                    return applet.isActive();
+                }
+            }, Timeout.timeout(ResourceManager.getPropInt("disposeTimeout")));
         }catch (NullPointerException e){
             throw new FixtureNotInitializedException("You need to call init() first to create applet and frame");
         }
