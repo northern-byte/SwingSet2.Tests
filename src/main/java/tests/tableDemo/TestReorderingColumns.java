3d74b08cc7efcab8b0dc6c3b22e6fe3cb382e827
Timur Barblishvili <unknitsplash@hotmail.com>
2017-11-18 00:35:24 +0300

    Add tests for reordering
    DragColumnUp to check that column will stay in place
    DragColumnUpAndToTheSide to check that we can drag column to the side above table and drop in into a new position
    TryDragCellUpAndToTheSide verifies that we can’t drag cells even with reordering enabled

--- src/main/java/tests/tableDemo/TestReorderingColumns.java
+++ src/main/java/tests/tableDemo/TestReorderingColumns.java
@@ -66,6 +66,89 @@ public class TestReorderingColumns {
         Assert.assertEquals(originalCellValue, newCellValue);
     }
 
+    /**
+     * Go to Table Demo
+     * Make sure, that reordering is allowed
+     * Create a new point based on the first one(subtract from y coordinate to make it higher)
+     * Get original index of column to be dragged
+     * Try to drag column up
+     * Get index of the column again
+     * Check that column is in the same place(same index)
+     */
+    @Test
+    public void DragColumnUp() {
+        String columnToDrag = spec.get("tableDemo.columnToDrag").String();
+
+        demo.allowReordering();
+
+        Point dragFrom = demo.getColumnHeaderPoint(columnToDrag);
+        Point dropTo = new Point(dragFrom.x, dragFrom.y - 100);
+
+        int startIndex = demo.getColumnIndex(columnToDrag);
+
+        demo.drapAndDrop(dragFrom, dropTo);
+
+        int endIndex = demo.getColumnIndex(columnToDrag);
+
+        Assert.assertEquals(startIndex, endIndex);
+    }
+
+    /**
+     * Go to Table Demo
+     * Make sure, that reordering is allowed
+     * Create a new point based on where we want to drag and subtract from y coordinate to make it higher
+     * Get original index of column to be dragged
+     * Try to drag column up and in desired direction
+     * Get index of the column again
+     * Check that column is in the desired place(by index)
+     */
+    @Test
+    public void DragColumnUpAndToTheSide() {
+        String columnToDrag = spec.get("tableDemo.columnWhereToDrag").String();
+        String columnWhereToDrop = spec.get("tableDemo.columnToDrag").String();
+
+        demo.allowReordering();
+
+        Point dragFrom = demo.getColumnHeaderPoint(columnToDrag);
+        Point dropTo = demo.getColumnHeaderPoint(columnWhereToDrop);
+        dropTo.y -= 100;
+
+        int whereToDropIndex = demo.getColumnIndex(columnWhereToDrop);
+
+        demo.drapAndDrop(dragFrom, dropTo);
+
+        int currentIndex = demo.getColumnIndex(columnToDrag);
+
+        Assert.assertEquals(currentIndex, whereToDropIndex);
+    }
+
+    /**
+     * Go to Table Demo
+     * Make sure, that reordering is allowed
+     * Get the point from where to drag cell
+     * Get the point where to drop the cell
+     * Try to drag cell
+     * Get position of the cell again
+     * Check that cell's position didn't change
+     */
+    @Test
+    public void TryDragCellUpAndToTheSide() {
+        String cellToDrag = spec.get("tableDemo.cellToDrag").String();
+        String cellWhereToDrop = spec.get("tableDemo.cellWhereToDrop").String();
+
+        demo.allowReordering();
+
+        Point dragFrom = demo.getCellPoint(cellToDrag);
+        Point dropTo = demo.getCellPoint(cellWhereToDrop);
+
+        demo.drapAndDrop(dragFrom, dropTo);
+
+        Point newPosition = demo.getCellPoint(cellToDrag);
+
+        Assert.assertEquals(dragFrom.x, newPosition.x);
+        Assert.assertEquals(dragFrom.y, newPosition.y);
+    }
+
     @Test //Prototype
     public void Prototype() {
         demo.clickTable();
