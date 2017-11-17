63d4f24f78dbb88fda9ba9b824ad44003117f6dc
Timur Barblishvili <unknitsplash@hotmail.com>
2017-11-17 04:32:56 +0300

    Add a test for dragging column to.a new position
    Positive test, where we drag one column over another and check that reordering actually happened

--- src/main/java/tests/tableDemo/TestReorderingColumns.java
+++ src/main/java/tests/tableDemo/TestReorderingColumns.java
@@ -6,6 +6,7 @@ import interfaces.fixtures.PrepareFixture;
 import interfaces.fixtures.SetupFixture;
 import interfaces.pageObjects.TableDemo;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import utils.Specification;
@@ -26,7 +27,39 @@ public class TestReorderingColumns {
     public void Close(){
         setupFixture.dispose();
     }
-    
+
+    /**
+     * Go to Table Demo
+     * Make sure, that reordering is allowed
+     * Get index of the column we want to drag
+     * Get index of column which place we'll occupy
+     * Load number of the row we'll use as for a check
+     * Get an original value of a cell in the column we are going to drag and specified row
+     * Drag and drop column to a new place
+     * Get value from the cell we projected our original value to be in
+     * Check if the values match
+     */
+    @Test
+    public void DragColumnToNewPosition(){
+        String columnToDrag = spec.get("tableDemo.columnToDrag").String();
+        String columnWhereToDrop = spec.get("tableDemo.columnWhereToDrag").String();
+
+        demo.allowReordering();
+
+        int startIndex = demo.getColumnIndex(columnToDrag);
+        int endIndex = demo.getColumnIndex(columnWhereToDrop);
+
+        int rowNumber = spec.get("tableDemo.reorderingRowNumber").Int();
+
+        String originalCellValue = demo.getStringFromTableCell(rowNumber, startIndex);
+
+        demo.drapAndDropColumnToColumn(columnToDrag, columnWhereToDrop);
+
+        String newCellValue = demo.getStringFromTableCell(rowNumber, endIndex);
+
+        Assert.assertEquals(originalCellValue, newCellValue);
+    }
+
     @Test //Prototype
     public void Prototype(){
         demo.clickTable();
