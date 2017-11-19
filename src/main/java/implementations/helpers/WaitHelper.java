f719a80aa4c5947ca2ac9e0e183eb26c5fe291b9
Timur Barblishvili <unknitsplash@hotmail.com>
2017-11-19 18:04:42 +0300

    Change wait helper method’s bound
    Make generic methods in wait helper bound to ComponentFixture, instead of JComponentFixture

--- src/main/java/implementations/helpers/WaitHelper.java
+++ src/main/java/implementations/helpers/WaitHelper.java
@@ -2,7 +2,7 @@ package implementations.helpers;
 
 import implementations.wrappers.Lazy;
 import org.fest.swing.exception.ComponentLookupException;
-import org.fest.swing.fixture.JComponentFixture;
+import org.fest.swing.fixture.ComponentFixture;
 import org.fest.swing.timing.Condition;
 import org.fest.swing.timing.Pause;
 import org.fest.swing.timing.Timeout;
@@ -11,11 +11,11 @@ import utils.ResourceManager;
 import java.util.function.Supplier;
 
 public class WaitHelper {
-    public <T extends JComponentFixture> Lazy<T> lazy(Supplier<T> supplier) {
+    public <T extends ComponentFixture> Lazy<T> lazy(Supplier<T> supplier) {
         return new Lazy<>(() -> active(supplier));
     }
 
-    public <T extends JComponentFixture> T active(Supplier<T> supplier) {
+    public <T extends ComponentFixture> T active(Supplier<T> supplier) {
         Pause.pause(new Condition("Wait until expected component is there") {
             @Override
             public boolean test() {
