605cb063ac74a7b96f503af5d23d0b6ef2b45ff1
Timur Barblishvili <unknitsplash@hotmail.com>
2017-11-20 14:02:56 +0300

    Add testing for column sorting
    Added tests for asc and desc sorting in a table columns.
    Tests go through different column value types, comparing Images, strings, doubles and colours.
    Added required properties to default specification.

---
+++ src/main/java/tests/tableDemo/TestSorting.java
@@ -0,0 +1,127 @@
+package tests.tableDemo;
+
+import implementations.fixtures.AppletSetupFixture;
+import implementations.fixtures.TableDemoPrepareFixture;
+import implementations.wrappers.TestImage;
+import interfaces.fixtures.PrepareFixture;
+import interfaces.fixtures.SetupFixture;
+import interfaces.pageObjects.TableDemo;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import utils.Specification;
+
+import java.awt.*;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(value = Parameterized.class)
+public class TestSorting {
+    private final SetupFixture setupFixture = new AppletSetupFixture();
+    private final PrepareFixture<TableDemo> prepareDemo = new TableDemoPrepareFixture();
+    private final static Specification spec = new Specification();
+    private TableDemo demo;
+
+    private String columnName;
+    private Object expectedAsc;
+    private Object expectedDesc;
+    private Class<?> contentType;
+
+    @Before
+    public void Setup() {
+        demo = prepareDemo.prepair(setupFixture.init());
+    }
+
+    @After
+    public void Close() {
+        setupFixture.dispose();
+    }
+
+    private static class TestData {
+        private static String textColumn = spec.get("tableDemo.textColumn").String();
+        private static String firstText = spec.get("tableDemo.firstText").String();
+        private static String lastText = spec.get("tableDemo.lastText").String();
+
+        private static String numberColumn = spec.get("tableDemo.numberColumn").String();
+        private static Double firstNumber = spec.get("tableDemo.firstNumber").Double();
+        private static Double lastNumber = spec.get("tableDemo.lastNumber").Double();
+
+        private static String colorColumn = spec.get("tableDemo.colorColumn").String();
+        private static Color firstColor = spec.get("tableDemo.firstColor").Color();
+        private static Color lastColor = spec.get("tableDemo.lastColor").Color();
+
+        private static String imageColumn = spec.get("tableDemo.imageColumn").String();
+        private static TestImage firstImage = spec.get("tableDemo.firstImage").TestImage();
+        private static TestImage lastImage = spec.get("tableDemo.lastImage").TestImage();
+    }
+
+    @Parameterized.Parameters(name = "{0}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {TestData.imageColumn, TestData.firstImage, TestData.lastImage, TestImage.class},
+                {TestData.textColumn, TestData.firstText, TestData.lastText, String.class},
+                {TestData.numberColumn, TestData.firstNumber, TestData.lastNumber, Double.class},
+                {TestData.colorColumn, TestData.firstColor, TestData.lastColor, Color.class}
+        });
+    }
+
+    public TestSorting(String columnName, Object expectedAsc, Object expectedDesc, Class<?> contentType) {
+        this.contentType = contentType;
+        this.columnName = columnName;
+        this.expectedAsc = expectedAsc;
+        this.expectedDesc = expectedDesc;
+    }
+
+    /**
+     * Get column, expected result and result type from parameters
+     * Go to Table Demo
+     * Click on column we want to sort, to sort it in asc order
+     * Check that actual content of first cell in that column corresponds to expected
+     */
+    @Test
+    public void TestSortingAsc() {
+        demo.clickColumnHeader(columnName);
+        int columnIndex = demo.getColumnIndex(columnName);
+        Object cast = getCast(demo.getValueFromCell(0, columnIndex), contentType);
+        Assert.assertNotNull(cast);
+        Assert.assertEquals((expectedAsc), cast);
+    }
+
+    /**
+     * Get column, expected result and result type from parameters
+     * Go to Table Demo
+     * Click on column we want to sort twice, to sort it in desc order
+     * Check that actual content of first cell in that column corresponds to expected
+     */
+    @Test
+    public void TestSortingDesc() {
+        demo.clickColumnHeader(columnName);
+        demo.clickColumnHeader(columnName);
+        int columnIndex = demo.getColumnIndex(columnName);
+        Object cast = getCast(demo.getValueFromCell(0, columnIndex), contentType);
+        Assert.assertNotNull(cast);
+        Assert.assertEquals((expectedDesc), cast);
+    }
+
+    private Object getCast(Object object, Class<?> type) {
+        if (type.isAssignableFrom(object.getClass())) {
+            return object;
+        }
+        try {
+            return type.getDeclaredConstructor(object.getClass()).newInstance(object);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
