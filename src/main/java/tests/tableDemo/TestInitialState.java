512ae4f8684fcd99d47b1f2125ea8cbdec50aa24
Timur Barblishvili <unknitsplash@hotmail.com>
2017-11-21 05:22:27 +0300

    Add test for initial state
    Test that features have correct state when app launches
    Added properties with initial values to spec

---
+++ src/main/java/tests/tableDemo/TestInitialState.java
@@ -0,0 +1,67 @@
+package tests.tableDemo;
+
+import implementations.fixtures.AppletSetupFixture;
+import implementations.fixtures.TableDemoPrepareFixture;
+import interfaces.fixtures.PrepareFixture;
+import interfaces.fixtures.SetupFixture;
+import interfaces.pageObjects.TableDemo;
+import org.junit.*;
+import org.junit.rules.Timeout;
+import utils.Platform;
+import utils.Specification;
+
+public class TestInitialState {
+    private final SetupFixture setupFixture = new AppletSetupFixture();
+    private final PrepareFixture<TableDemo> prepareDemo = new TableDemoPrepareFixture();
+    private final Specification spec = new Specification();
+    private TableDemo demo;
+
+    @Rule
+    public Timeout globalTimeout = Timeout.millis(Platform.getConfigProp("testTimeout").Int());
+
+    @Before
+    public void Setup() {
+        demo = prepareDemo.prepair(setupFixture.init());
+    }
+
+    @After
+    public void Close() {
+        setupFixture.dispose();
+    }
+
+    private class TestData {
+        boolean reordering = spec.get("tableDemo.initialReordering").Boolean();
+        boolean horizLines = spec.get("tableDemo.initialHorizLines").Boolean();
+        boolean vertLines = spec.get("tableDemo.initialVertLines").Boolean();
+        boolean rowSelection = spec.get("tableDemo.initialRowSelection").Boolean();
+        boolean columnSelection = spec.get("tableDemo.initialColumnSelection").Boolean();
+        int intercellSpacing = spec.get("tableDemo.initialIntercellSpacing").Int();
+        int rowHeight = spec.get("tableDemo.initialRowHeight").Int();
+        String selectionMode = spec.get("tableDemo.initialSelectMode").String();
+        String autoresizeMode = spec.get("tableDemo.initialResizeMode").String();
+    }
+
+    @Test
+    public void TestInitialState(){
+        TestData testData = new TestData();
+        boolean reordering = demo.isReorderingAllowed();
+        boolean horizLines = demo.areHorizontalLinesEnabled();
+        boolean vertLines = demo.areVerticalLinesEnabled();
+        boolean rowSelection = demo.isRowSelectionEnabled();
+        boolean columnSelection = demo.isColumnSelectionEnabled();
+        int intercellSpacing = demo.getIntercellSpacingValue();
+        int rowHeight = demo.getRowHeightValue();
+        String selectionMode = demo.getSelectionMode();
+        String autoresizeMode = demo.getAutoresizeMode();
+
+        Assert.assertEquals(reordering,testData.reordering);
+        Assert.assertEquals(horizLines, testData.horizLines);
+        Assert.assertEquals(vertLines, testData.vertLines);
+        Assert.assertEquals(rowSelection, testData.rowSelection);
+        Assert.assertEquals(columnSelection, testData.columnSelection);
+        Assert.assertEquals(intercellSpacing, testData.intercellSpacing);
+        Assert.assertEquals(rowHeight, testData.rowHeight);
+        Assert.assertEquals(selectionMode, testData.selectionMode);
+        Assert.assertEquals(autoresizeMode, testData.autoresizeMode);
+    }
+}
